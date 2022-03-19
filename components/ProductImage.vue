654bc58c9f01a3612431d9d51123c88aa730d653
Idris Olubisi <heedris2olubisi@gmail.com>
2022-03-19 23:30:25 +0100

    File upload

--- components/ProductImage.vue
+++ components/ProductImage.vue
@@ -61,10 +61,10 @@ export default {
         ],
       },
       zoomerOptions: {
-        zoomFactor: 4, // scale for zoomer
+        zoomFactor: 3, // scale for zoomer
         pane: "pane", // three type of pane ['pane', 'container-round', 'container']
         hoverDelay: 300, // how long after the zoomer take effect
-        namespace: "zoomer", // add a namespace for zoomer component, useful when on page have multiple zoomer
+        // namespace: "zoomer", // add a namespace for zoomer component, useful when on page have multiple zoomer
         move_by_click: false, // move image by click thumb image or by mouseover
         scroll_items: 5, // thumbs for scroll
         choosed_thumb_border_color: "#bbdefb", // choosed thumb border color
